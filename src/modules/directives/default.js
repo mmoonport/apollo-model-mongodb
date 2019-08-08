3ee473a8da73b4db9b168ccae00036513947e579
Matt Moon <matt@feltapp.com>
2019-08-07 21:37:49 -0500

    setting default value on resolve

--- src/modules/directives/default.js
+++ src/modules/directives/default.js
@@ -1,9 +1,9 @@
-import {SchemaDirectiveVisitor} from 'graphql-tools';
+import { SchemaDirectiveVisitor } from 'graphql-tools';
 
-import {appendTransform, reduceTransforms} from '../../inputTypes/utils';
-import {fieldInputTransform} from '../../inputTypes/transforms';
-import {TRANSFORM_TO_INPUT} from '../../inputTypes/handlers';
-import {CREATE} from '../../inputTypes/kinds';
+import { appendTransform, reduceTransforms } from '../../inputTypes/utils';
+import { fieldInputTransform } from '../../inputTypes/transforms';
+import { TRANSFORM_TO_INPUT } from '../../inputTypes/handlers';
+import { CREATE } from '../../inputTypes/kinds';
 
 export const typeDef = `directive @default(value: String!) on FIELD_DEFINITION`;
 
@@ -28,6 +28,15 @@ class DefaultDirective extends SchemaDirectiveVisitor {
         },
       ],
     });
+
+    let resolve = field.resolve;
+    field.resolve = async (parent, args, context, info) => {
+      let result = await resolve(parent, args, context, info);
+      if (result === undefined || result === null) {
+        result = value;
+      }
+      return result;
+    };
   }
 
   _setDefaultValue = (fieldName, defaultValue) => params => {
